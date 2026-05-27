6114a641a48833da53deb17d882bb6a1c4c1be10
victor-lopes-developer <victorlopesdeveloper@gmail.com>
2026-05-27 16:10:33 -0300

    Testando tipos de variáveis

---
+++ aula05/script.js
@@ -0,0 +1,7 @@
+let idade = 28 // typo number
+let nome = 'Victor Lopes Moreira' // type string
+let estudandoProg = true // type boolean
+
+console.log(idade)
+console.log(nome)
+console.log(estudandoProg)

d426e566bf6ac53cf62053a7e30fa77fc99410bc
victor-lopes-developer <victorlopesdeveloper@gmail.com>
2026-05-28 13:59:58 -0300

    ajuste nos arquivos da aula de operadores

---
+++ aula07/aritimeticos.js
@@ -0,0 +1,18 @@
+//OPERADORES ARITMÉTICOS
+
+var soma = 5 + 2 // (+) Operador de soma // resultado: 5 + 2 = 7
+var subtracao = 5 - 2 // (-) Operador de subtração // resultado: 5 - 2 = 3
+var multiplicacao = 5 * 2 // (*) Operador de multiplicação // resultado: 5 * 2 = 10
+var divisao = 5 / 2 // (/) Operador de divisão //resultado: 5 / 2 = 2.5
+var modulo = 5 % 2 // (%) Operador de módulo (ou resto de divisão) // resultado: 5 % 2 = 1 (cabe duas vezes 2 no cinco e sobra 1 que é o modulo da operação )
+var exponenciacao = 5 ** 2 // Operador de exponenciação (ou potenciação) // resultado: 5 ** 2 = 25 ( 5 * 5 = 25, o mesmo que 5 elevado a 2)
+
+//PRECEDÊNCIA DA PRIORIDADE DOS OPERADORES NAS OPERAÇÕES 
+
+/*
+1º.  ()
+2º.  **
+3º.  * / %
+4º.  + -
+*/
+

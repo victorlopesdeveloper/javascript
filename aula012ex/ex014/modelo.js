95aaac1c74004baa897c84d21ff65510615f7fd2
Victor Lopes <victorlopesdeveloper@gmail.com>
2026-06-02 20:10:42 -0300

    primeiro exercício completo

---
+++ aula012ex/ex014/modelo.js
@@ -0,0 +1,29 @@
+function carregar(){
+
+var msg = document.querySelector('#msg')
+
+var foto = document.querySelector('#imagem')
+
+var data = new Date()
+
+var hora = data.getHours()
+
+msg.innerHTML = `Agora são ${hora} horas`
+
+if(hora >=0 && hora < 12){
+    //bom dia
+    foto.src = 'manha-circular.webp'
+    document.body.style.background = '#f5cf6e'
+} else if(hora < 18){
+    //boa tarde
+    foto.src = 'tarde-circular.webp'
+    document.body.style.background = '#7F81AA'
+}else{
+    //boa noite
+    foto.src = 'noite-circular.webp'
+    document.body.style.background = '#141917'
+
+}
+
+
+}

1976d2cd7f7bb86298a540ce370dfdf19c7c5ff5
victor-lopes-developer <victorlopesdeveloper@gmail.com>
2026-06-03 10:09:43 -0300

    ajuste

--- aula013/ambiente.js
+++ aula013/ambiente.js
@@ -6,12 +6,7 @@ let c = 1
 do{
     console.log(`Passo ${c}`)
     c++
-}while (c <= 2)
-
-
-
-
-
+}while (c <= 2);
 
 /*
 Os dois fazem a mesma coisa, nesse estrutura de repetição enquanto a variavel c for menor ou igual a 6 ele vai executar o console.log('Tudo bem?'), isso se chama laços de repetição (com teste lógico no início)
@@ -28,3 +23,10 @@ console.log('Tudo bem?')
 console.log('Tudo bem?')
 console.log('Tudo bem?')
 */
+
+
+
+
+
+
+

15d225c118922e019fa79b1f5df69c4acf12f95a
victor-lopes-developer <victorlopesdeveloper@gmail.com>
2026-06-03 17:00:35 -0300

    exercício tabuada completo

--- aula014ex/ex017/modelo.js
+++ aula014ex/ex017/modelo.js
@@ -1,1 +1,16 @@
-alert('Olá')
+function tabuada(){
+    let num = document.querySelector('#inum')
+    let tab = document.querySelector('#seltab')
+    if(num.value.length == 0){
+        window.alert('Por favor, digite um número!') 
+    }else{
+        let n = Number(num.value)
+        tab.innerHTML = ''
+        for(let c = 1; c <= 10; c++ ){
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+        }
+    }
+}

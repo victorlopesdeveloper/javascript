d991a3ead2602b90797bb75c96f137237f343931
victor-lopes-developer <victorlopesdeveloper@gmail.com>
2026-06-03 10:32:07 -0300

    aprendendo o uso de for nos laços de repetição

---
+++ aula014/ambiente.js
@@ -0,0 +1,15 @@
+//LAÇOS DE REPETIÇÃO USANDO O for
+
+/*
+var c = 1
+while(c <= 5){
+    console.log(c)
+    c++
+}
+*/
+// O FOR FAZ TEM A MESMA FUNCIONALIDADE DO CÓDIGO ACIMA
+console.log('Vai começar...')
+for(var c = 1; c <= 8; c++){
+    console.log(c)
+}
+console.log('FIM!')
